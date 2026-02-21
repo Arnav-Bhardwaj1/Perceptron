d75b19d67dff2abd65e87f0a51edfb942bd6c07e
Arnav-Bhardwaj1 <arnavbhardwaj111@gmail.com>
2026-02-22 01:41:20 +0530

    implemented monorepo setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -6,10 +6,10 @@ export default function Page() {
   const users = useQuery(api.users.getMany);
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>apps/web</p>
+      <p>apps/widget</p>
       <div className="max-w-sm w-full mx-auto">
         {JSON.stringify(users, null, 2)}
       </div>
     </div>
   )
-}
+} 
